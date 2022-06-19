5f207f311517dc43c029dd6d0ae3439a692b80b8
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-06-20 00:01:14 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482','6287877173955','0']
-global.ownerName = 'Fandyyy X All Creator'
-global.packname = 'Ichigo Kurosaki'
-global.author = 'Whatsapp Bot 2022'
+global.ownerNumber = ['6281322577123','6281322577123','6281322577123', '6281322577123']
+global.ownerName = 'URFAVBOY-LINTANG'
+global.packname = 'URFAVBOY-LINTANG'
+global.author = 'KAMAGAPO BOT'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
 
